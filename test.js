5fea193bfc8a857a592f2ada8fd92fa296e17a13
Marcus Bennett <marcus@minimalmar.com>
2018-07-22 08:57:29 -0400

    bug fixes

--- test.js
+++ test.js
@@ -1,13 +1,3 @@
-var individualChoice1 = document.getElementById("individual-choice1");
-
-var background1 = document.getElementById("background-image1");
-var background2 = document.getElementById("background-image2");
-var background3 = document.getElementById("background-image3");
-
-var isToggled = false;
-individualChoice1.addEventListener('mouseover', {
-  handleEvent: function (event) {
-      background1.classList.toggle("purple")
-          //background1.classList.toggle("background-image1-hover");
-  }
+$('.carousel').carousel({
+  interval: 3000
 });
